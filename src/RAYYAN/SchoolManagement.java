c4e463eb847431863f105257d307c478b8abd30b
devbjack <devbjack@gmail.com>
2025-11-25 19:41:56 +0300

    case D, runD() method

--- src/RAYYAN/SchoolManagement.java
+++ src/RAYYAN/SchoolManagement.java
@@ -2,7 +2,6 @@ package RAYYAN;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class SchoolManagement {
@@ -16,9 +15,9 @@ public class SchoolManagement {
     // data storage
     private HashMap<Integer, Teacher> teachers;
     private HashMap<Integer, SupportStaff> supportStaffs;
-    private Classroom[] classes;
+    private HashMap<Integer, Classroom> classes;
     private HashMap<Integer, Department> departments;
-    private ArrayList<Student> students;
+    private HashMap<Integer, Student> students;
 
     public SchoolManagement(String schoolName, String address, int contactNumber, String mediumOfStudy) {
         this.schoolName = schoolName;
@@ -74,12 +73,18 @@ public class SchoolManagement {
     public void initialize(/* Auditorium auditorium, Playground playground, */NoticeBoard noticeBoard,
             Employee[] employees, Classroom[] classes/* Lab[] labs */) {
         this.noticeBoard = noticeBoard;
-        this.classes = classes;
+        initClasses(classes);
         getTeacherFromEmployee(employees);
         getSupportStaffFromEmployee(employees);
-        students = new ArrayList<>();
+        students = new HashMap<>();
         initDept();
         initInChargeDept();
+    }
+
+    private void initClasses(Classroom[] classArr){
+        for (int i = 0; i < classArr.length; i++) {
+            classes.put(classArr[i].getClassId(), classArr[i]);
+        }
     }
 
     private void initDept() {
@@ -92,12 +97,13 @@ public class SchoolManagement {
             this.departments.put(departments[i].getDepartmentId(), departments[i]);
         }
     }
-    private void initInChargeDept(){
-        for (Map.Entry<Integer, Department> department : departments.entrySet()) {
-            for (Map.Entry<Integer, Teacher> teacher : teachers.entrySet()) {
-                if(teacher.getValue().getDepartmentId() == department.getValue().getDepartmentId()){
-                    if(department.getValue().getInchargeName().isEmpty()){
-                        department.getValue().setInchargeName(teacher.getValue().getEmployeeName());
+
+    private void initInChargeDept() {
+        for (Department department : departments.values()) {
+            for (Teacher teacher : teachers.values()) {
+                if (teacher.getDepartmentId() == department.getDepartmentId()) {
+                    if (department.getInchargeName().isEmpty()) {
+                        department.setInchargeName(teacher.getEmployeeName());
                     }
                 }
             }
@@ -149,11 +155,8 @@ public class SchoolManagement {
                     break;
 
                 case "D":
-                    System.out.println("[D] Class");
-                    System.out.println("\t1. Add Students");
-                    System.out.println("\t2. Assign Teacher");
-                    System.out.println("\t3. Show Details");
-                    System.out.println("\t4. Go Back");
+                    runD();
+                    break;
 
                 case "E":
                     System.out.println("[E] Notice Board");
@@ -181,16 +184,16 @@ public class SchoolManagement {
     }
 
     private void runB() {
-        int input2 = 0;
-        while (input2 != 4) {
+        int input1 = 0;
+        while (input1 != 4) {
             System.out.println("[B] Student");
             System.out.println("\t1. Add Student");
             System.out.println("\t2. Show Student Details");
             System.out.println("\t3. Pay Fee");
             System.out.println("\t4. Go Back");
-            input2 = sc.nextInt();
+            input1 = sc.nextInt();
 
-            if (input2 == 1) {
+            if (input1 == 1) {
                 System.out.print("Student ID: ");
                 int studentId = sc.nextInt();
                 System.out.print("\nName: ");
@@ -201,117 +204,170 @@ public class SchoolManagement {
                 int section = sc.nextInt();
                 System.out.print("\nBus ID: ");
                 int busId = sc.nextInt();
-                students.add(new Student(studentId, name, classId, section, busId));
-            } else if (input2 == 2) {
+                students.put(studentId, new Student(studentId, name, classId, section, busId));
+            } else if (input1 == 2) {
                 System.out.println("List of students:");
                 for (int i = 0; i < students.size(); i++) {
                     System.out.println((i + 1) + ". " + students.get(i).getStudentName());
                 }
                 System.out.println(students.size() + ". Show All");
                 System.out.println("Choose which student you want to see the details");
-                int input3 = sc.nextInt();
-                if (input3 == students.size()) {
-                    for (Student std : students) {
+                int input2 = sc.nextInt();
+                if (input2 == students.size()) {
+                    for (Student std : students.values()) {
                         System.out.println(twolines);
                         System.out.println(std.studentDetails());
                     }
                 } else {
-                    System.out.println(students.get(input3 - 1).studentDetails());
+                    System.out.println(students.get(input2 - 1).studentDetails());
                 }
-            } else if (input2 == 3) {
+            } else if (input1 == 3) {
                 // pay fee
+            } else {
+                System.out.println("Option not found!");
             }
         }
     }
 
     private void runC() {
-        int input2 = 0;
-        while (input2 != 3) {
+        int input1 = 0;
+        while (input1 != 3) {
             System.out.println("[C] Employee");
             System.out.println("\t1. Teacher");
             System.out.println("\t2. Support Staff");
             System.out.println("\t3. Go Back");
 
-            if (input2 == 1) {
-                String input3 = "";
-                while (input3.equalsIgnoreCase("c")) {
+            if (input1 == 1) {
+                String input2 = "";
+                while (input2.equalsIgnoreCase("c")) {
                     System.out.println("1. Teacher");
                     System.out.println("\ta. Show Teacher Details");
                     System.out.println("\tb. Receive Salary");
                     System.out.println("\tc. Go Back");
-                    input3 = sc.next();
-                    if (input3.equalsIgnoreCase("a")) {
+                    input2 = sc.next();
+                    if (input2.equalsIgnoreCase("a")) {
                         System.out.println("List of Teacher:");
                         int numbering = 1;
-                        for (int i = 0; i < teachers.size(); i++) {
-
-                            System.out.println(numbering++ + ". " + teachers[i].getEmployeeName());
-
-                        }
-                        for (Teac\ classroom : classes) {
-                            
+                        for (Teacher teacher : teachers.values()) {
+                            System.out.println(
+                                    numbering++ + ". " + teacher.getEmployeeId() + " " + teacher.getEmployeeName());
                         }
                         System.out.println(numbering + ". Show All");
-                        System.out.println("Choose which teacher you want to see the details");
-                        int input4 = sc.nextInt();
-                        if (input4 == numbering) {
+                        System.out.println("Choose which teacher you want to see the details (input ID)");
+                        int input3 = sc.nextInt();
+                        if (input3 == numbering) {
                             numbering = 1;
-                            for (int i = 0; i < teachers.length; i++) {
+                            for (HashMap.Entry<Integer, Teacher> teacher : teachers.entrySet()) {
                                 System.out.println(twolines);
-                                System.out.println(numbering++ + ". " + teachers[i].employeeDetails());
-                                // need to map employee with department
-
+                                System.out.print(numbering++ + ". " + teacher.getValue().employeeDetails());
+                                System.out.println(departments.get(teacher.getKey()));
                             }
                         } else {
-                            try {
-                                System.out.println(teachers[input4]);
-                            } catch (ArrayIndexOutOfBoundsException e) {
+                            Teacher teacher = teachers.get(input3);
+                            if (teacher == null) {
                                 System.out.println("Teacher not found");
+                            } else {
+                                System.out.print(teacher.employeeDetails());
+                                System.out.println(departments.get(teacher.getDepartmentId()));
                             }
                         }
-                    } else if (input3.equalsIgnoreCase("b")) {
+                    } else if (input2.equalsIgnoreCase("b")) {
                         // receive salary
+                    } else {
+                        System.out.println("Option not found!");
                     }
                 }
-            } else if (input2 == 2) {
-                String input3 = "";
-                while (input3.equalsIgnoreCase("c")) {
+            } else if (input1 == 2) {
+                String input2 = "";
+                while (input2.equalsIgnoreCase("c")) {
                     System.out.println("1. Support Staff");
                     System.out.println("\ta. Show Support Staff Details");
                     System.out.println("\tb. Receive Salary");
                     System.out.println("\tc. Go Back");
-                    input3 = sc.next();
-                    if (input3.equalsIgnoreCase("a")) {
+                    input2 = sc.next();
+                    if (input2.equalsIgnoreCase("a")) {
                         System.out.println("List of Support Staff:");
                         int numbering = 1;
-                        for (int i = 0; i < supportStaffs.length; i++) {
-
-                            System.out.println(numbering++ + ". " + supportStaffs[i].getEmployeeName());
-
+                        for (SupportStaff supportStaff : supportStaffs.values()) {
+                            System.out.println(
+                                    numbering++ + ". " + supportStaff.getEmployeeId() + " "
+                                            + supportStaff.getEmployeeName());
                         }
                         System.out.println(numbering + ". Show All");
-                        System.out.println("Choose which support staff you want to see the details");
-                        int input4 = sc.nextInt();
-                        if (input4 == numbering) {
+                        System.out.println("Choose which support staff you want to see the details (input ID)");
+                        int input3 = sc.nextInt();
+                        if (input3 == numbering) {
                             numbering = 1;
-                            for (int i = 0; i < supportStaffs.length; i++) {
-
+                            for (HashMap.Entry<Integer, SupportStaff> supportStaff : supportStaffs.entrySet()) {
                                 System.out.println(twolines);
-                                System.out.println(numbering++ + ". " + supportStaffs[i].employeeDetails());
-                                // need to map employee with department
-
+                                System.out.print(numbering++ + ". " + supportStaff.getValue().employeeDetails());
+                                System.out.println(departments.get(supportStaff.getKey()));
                             }
                         } else {
-                            try {
-                                System.out.println(supportStaffs[input4]);
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                                System.out.println("Support Staff not found");
+                            SupportStaff supportStaff = supportStaffs.get(input3);
+                            if (supportStaff == null) {
+                                System.out.println("Teacher not found");
+                            } else {
+                                System.out.print(supportStaff.employeeDetails());
+                                System.out.println(departments.get(supportStaff.getDepartmentId()));
                             }
                         }
-                    } else if (input3.equalsIgnoreCase("b")) {
+                    } else if (input2.equalsIgnoreCase("b")) {
                         // receive salary
+                    } else {
+                        System.out.println("Option not found!");
                     }
                 }
+            } else {
+                System.out.println("Option not found!");
+            }
+        }
+    }
+
+    private void runD() {
+        int input1 = 0;
+        while (input1 != 4) {
+            System.out.println("[D] Class");
+            System.out.println("\tChoose which class do you want to edit (Class ID)");
+            int numbering = 1;
+            for (HashMap.Entry<Integer, Classroom> classroom : classes.entrySet()) {
+                System.out.println(numbering++ + ". " + classroom.getKey() + " " + classroom.getValue().getClassName());
+            }
+            int cID = sc.nextInt();
+            Classroom classroom = classes.get(cID);
+            if(classroom == null){
+                System.out.println("Classroom not found!");
+                continue;
+            }
+
+            System.out.println("Class " + classroom.getClassName());
+            System.out.println("\t1. Add Students");
+            System.out.println("\t2. Assign Teacher");
+            System.out.println("\t3. Show Details");
+            System.out.println("\t4. Go Back");
+            if(input1 == 1){
+                System.out.print("Input Student ID you want to assign to the class: ");
+                int stdID = sc.nextInt();
+                while(students.get(stdID) == null){
+                    System.out.println("Student not Found!");
+                    stdID = sc.nextInt();
+                }
+                students.get(stdID).setClassId(classroom.getClassId());
+            }
+            else if(input1 == 2){
+                System.out.print("Input Teacher ID you want to assign to the class: ");
+                int tchID = sc.nextInt();
+                while(teachers.get(tchID) == null){
+                    System.out.println("Teacher not Found!");
+                    tchID = sc.nextInt();
+                }
+                classroom.setTeacherId(tchID);
+            }
+            else if(input1 == 3){
+                System.out.println(classroom.classDetails());
+            }
+            else{
+                System.out.println("Option not found!");
             }
         }
     }
