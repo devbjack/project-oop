d58159602273322e67c06b025106db34faeac162
Beeyour <65689511+Beeyour@users.noreply.github.com>
2025-11-26 16:19:21 +0300

    a
    a

--- src/OMAR/LabEquipments.java
+++ src/OMAR/LabEquipments.java
@@ -1,5 +1,5 @@
 package OMAR;
-public class LabEquipments {
+public class LabEquipments extends Equipments{
     private int equipmentCount;
     private String equipmentName;
     
@@ -7,9 +7,39 @@ public class LabEquipments {
         this.equipmentName = equipmentName;
         this.equipmentCount = equipmentCount;
 
-
-        
     }
+    
+    
+    
+    @Override
+    public String equipmentDetails(){
+        return String.format(
+            "%-1s: %d\n" +  
+            "%-1s: %d\n" +
+            "%-1d: %s\n" +  
+            "%-1s: %d\n" 
+            ,     
+            "Equipment ID", this.getEquipmentId(),
+            "Cost: ", this.getCost(),
+            "Equipment Name: ", equipmentName,
+            "Equipment Count: ", equipmentCount
+        );
+    }
+
+    @Override
+    public void purchaseEquipment(int newCount, int newCost) {
+        this.equipmentCount += newCount;
+        super.setCost(newCost);
+        System.out.println("Purchased " + newCount + " of " + equipmentName);
+    }
+
+    @Override
+    public void repair() {
+        System.out.println("Repairing Lab Equipment: " + equipmentName);
+        System.out.println("Status check: Calibrating...");
+    }
+
+
     // setter
     public void setEquipmentCount(int equipmentCount){
         this.equipmentCount = equipmentCount;
@@ -17,7 +47,6 @@ public class LabEquipments {
     public void setEquipmentName(String equipmentName){
         this.equipmentName = equipmentName;
     }
-
 
     // getter
     public int getEquipmentCount(){
