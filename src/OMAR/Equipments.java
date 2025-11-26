63c9d869e33d5805d1d3c08c397aa4a5b53efd4c
Beeyour <65689511+Beeyour@users.noreply.github.com>
2025-11-26 16:19:02 +0300

    a
    a

--- src/OMAR/Equipments.java
+++ src/OMAR/Equipments.java
@@ -3,12 +3,33 @@ public class Equipments {
     private int equipmentId, cost;
 
     public Equipments(){
+
     }
+
+    
+    
+    public String equipmentDetails(){
+        return String.format(
+            "%-1s: %d\n" +  
+            "%-1s: %d\n",     
+            "Equipment ID", equipmentId,
+            "Cost: ", cost
+        );
+    }
+
+    public void purchaseEquipment(int count, int cost){
+
+    }
+
+    public void repair(){
+
+    }
+
+
     // setter
     public void setEquipmentId(int equipmentId){
         this.equipmentId = equipmentId;
-    }
-    
+    }  
     public void setCost(int cost){
         this.cost = cost;
     }
